4f0c8a5af7bd2946b06574458f02bcc898e07ec6
Anthony <ajxs@panoptic.online>
2026-08-11 09:02:41 +1000

    Resolve issue with incorrectly printing log statements with no logging tags

--- src/kernel/src/logging.adb
+++ src/kernel/src/logging.adb
@@ -23,7 +23,7 @@ package body Logging is
    is (Level = Log_Level_Error
        or else
          (Level <= System_Logging_Level
-          and then (Tags'Length = 0 or else Are_Given_Tags_Active (Tags))));
+          and then Are_Given_Tags_Active (Tags)));
 
    procedure Log_Message
      (Message : String; Tags : Log_Tags; Level : Log_Level_T) is
@@ -38,10 +38,6 @@ package body Logging is
    procedure Log_Message_Wide
      (Wide_Message : Wide_String; Tags : Log_Tags; Level : Log_Level_T) is
    begin
-      if not Should_Log_To_Debug_Console (Tags, Level) then
-         return;
-      end if;
-
       declare
          Message : String (1 .. Wide_Message'Length);
       begin
@@ -49,7 +45,9 @@ package body Logging is
             Message (I) := Convert_Wide_Char_To_ASCII (Wide_Message (I));
          end loop;
 
-         if Active_Logging_Transports (Log_Transport_Debug_Console) then
+         if Active_Logging_Transports (Log_Transport_Debug_Console)
+           and then Should_Log_To_Debug_Console (Tags, Level)
+         then
             Log_To_Debug_Console (Message, Level);
          end if;
       end;
